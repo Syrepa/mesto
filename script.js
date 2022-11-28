cabbb274f6bdeec46cb5d9baf4abc6a36136812c
Nikita Galiev <nixon.2134@gmail.com>
2022-11-29 00:09:08 +0600

    feat: коддинг js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 let popupСlose = document.querySelector(".popup__close");
-let popupOpened = document.querySelector(".popup_opened");
-let profileEditBtn = document.querySelector(".profile_edit");
+let popupOpened = document.querySelector(".popup");
+let profileEditBtn = document.querySelector(".profile__edit-btn");
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
+
+function openPopup() {
+  popupOpened.classList.add("popup_opened");
+}
+
+profileEditBtn.addEventListener("click", openPopup);
+
+function closePopup() {
+  popupOpened.classList.remove("popup_opened");
+}
+
+popupСlose.addEventListener("click", closePopup);
+
+let profileName = profileTitle.textContent;
+
+console.log(2 + 1);
