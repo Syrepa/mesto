fecaf296f72681de5c65c69a6012135ca0911218
Nikita Galiev <nixon.2134@gmail.com>
2022-11-29 15:26:29 +0600

    feat: разработка кнопки отправики js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let popupOpened = document.querySelector(".popup");
 let profileEditBtn = document.querySelector(".profile__edit-btn");
 let profileTitle = document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
+let popupInputName = document.querySelector(".popup__name");
+let popupInputJob = document.querySelector(".popup__job");
 
 function openPopup() {
   popupOpened.classList.add("popup_opened");
@@ -17,5 +19,31 @@ function closePopup() {
 popupСlose.addEventListener("click", closePopup);
 
 let profileName = profileTitle.textContent;
+let profileJob = profileSubtitle.textContent;
 
-console.log(2 + 1);
+popupInputName.setAttribute("placeholder", profileName);
+popupInputJob.setAttribute("placeholder", profileJob);
+
+// Находим форму в DOM
+let formElement = document.querySelector(".popup__submit-btn");
+// Находим поля формы в DOM
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+
+  // Получите значение полей jobInput и nameInput из свойства value
+  let popupName = popupInputName.value;
+  let popupJob = popupInputJob.value;
+  // Выберите элементы, куда должны быть вставлены значения полей
+  profileTitle.textContent = popupName;
+  profileSubtitle.textContent = popupJob;
+  // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener("submit", handleFormSubmit);
